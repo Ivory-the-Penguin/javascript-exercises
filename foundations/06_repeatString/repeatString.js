341f9c06b858f7bae0456c5dedcc3607f684eff2
Ivory <ivory-the-penguin@proton.me>
2026-06-01 21:22:55 +0100

    repeat string done

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  if (num < 0) {
+    return "ERROR";
+  }
+  if (num === 0) {
+    return "";
+  }
 
+  let out = "";
+  for (i = 0; i < num; i++) {
+    out += string;
+  }
+  return out;
 };
 
 // Do not edit below this line
