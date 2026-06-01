8d902de4229fbb32bdd9364805ac3d75d21469ca
Ivory <ivory-the-penguin@proton.me>
2026-06-01 21:40:52 +0100

    remove from array done

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+  return arr.filter((num) => {
+    for (let item of args) {
+      if (num === item) {
+        return false;
+      }
+    }
+
+    return true;
+  });
 };
 
 // Do not edit below this line
