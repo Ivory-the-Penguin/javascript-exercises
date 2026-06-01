38ed6b83ef0e2ca4decdcd6dab6ce86686b9b5cb
Ivory <ivory-the-penguin@proton.me>
2026-06-01 21:27:02 +0100

    reverse string done

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let out = "";
+  for (i = string.length - 1; i >= 0; i--) {
+    out += string[i];
+  }
+  return out;
 };
 
 // Do not edit below this line
