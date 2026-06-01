704210ac1a79819c1ee403761808a2194f956574
Ivory <ivory-the-penguin@proton.me>
2026-06-01 21:49:57 +0100

    sum all done

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+    return "ERROR";
+  }
+  if (num1 < 0 || num2 < 0) {
+    return "ERROR";
+  }
 
+  let smaller, bigger;
+
+  if (num1 >= num2) {
+    bigger = num1;
+    smaller = num2;
+  } else {
+    bigger = num2;
+    smaller = num1;
+  }
+
+  let sum = 0;
+  for (i = smaller; i <= bigger; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
