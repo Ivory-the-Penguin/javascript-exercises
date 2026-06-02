aaa5a56e7a56d392946119f375d46a5dfa76527a
Ivory <ivory-the-penguin@proton.me>
2026-06-02 15:47:43 +0100

    temperture conversion done

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,18 @@
-const convertToCelsius = function() {
+function roundDecimal(num, digits) {
+  pow = Math.pow(10, digits);
+  return Math.round(num * pow) / pow;
+}
+
+const convertToCelsius = function (num) {
+  return roundDecimal((num - 32) * (5 / 9), 1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (num) {
+  return roundDecimal(num * (9 / 5) + 32, 1);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
