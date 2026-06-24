2a6d583d7f7a24d4afd4369013cd5bd8ae36e428
Ivory <ivory-the-penguin@proton.me>
2026-06-24 14:38:07 +0100

    Plaindromes done

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let strarr = Array.from(str)
+    .filter((c) => !/[^A-Za-z0-9]/.test(c))
+    .map((c) => c.toLowerCase());
+  console.log(strarr);
 
+  let reversed = strarr.slice().reverse();
+  console.log(reversed);
+
+  return strarr.every((val, index) => val === reversed[index]);
 };
 
 // Do not edit below this line
